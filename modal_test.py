8ee1cdb965d6aad0a57b3edea67c3c4e94c17f01
Rielly Griffiths <rgriffiths+gitlab@extensiv.com>
2024-02-29 10:22:16 -0700

    new test

--- modal_test.py
+++ modal_test.py
@@ -1,31 +1,55 @@
-# import modal
-# import sys
-from prefect import flow
+import polars as pl
+import requests
+from prefect import flow, task
+from prefect.blocks.system import Secret
 
-# stub = modal.Stub('example-test')
+@task
+def get_data():
+    user = 'rgriffiths@extensiv.com'
+    secret_block = Secret.load("jira-api-token")
+    pwd = secret_block.get()
+    query = 'issuetype = Bug AND priority in (P3-30, P3-60) order by created DESC'
+    url = f'https://3plcentral.atlassian.net/rest/api/2/search?maxResults=200&jql={query}'
+    r=requests.get(url,auth=(user,pwd)).json()
+    rows = []
 
-# @stub.function()
-# def f(i):
-#     if i % 2 == 0:
-#         print('hello', i)
-#     else:
-#         print('world',i, file=sys.stderr)
-#     return i*i
+    for issue in r['issues']:
+        key = f"'{issue['key']}'" 
+        issue_type = f"'{issue['fields']['issuetype']['name']}'" 
+        summary = f"'{issue['fields']['summary']}'".replace("'","")
+        summary = f"'{summary}'"
+        priority = f"'{issue['fields']['priority']['name']}'"
+        product = issue['fields']['customfield_11612']
+        if product:
+            product=f"'{product['value']}'"
+        comp=[]
+        for component in issue['fields']['components']:
+            comp.append(component['name'])
+        component = ', '.join(comp)
+        component = f"'{component}'" 
+        created = f"'{issue['fields']['created']}'"
+        sla_escalation = f"'{issue['fields']['customfield_11647']}'"
+        resolved = f"'{issue['fields']['resolutiondate']}'"
+        due = f"'{issue['fields']['customfield_11685']}'"
+        status = f"'{issue['fields']['status']['name']}'"
+        if not product:
+            product='Null'
+        if sla_escalation == "'None'":
+            sla_escalation = 'Null'
+        if resolved == "'None'":
+            resolved = 'Null'
+        if due == "'None'":
+            due = 'Null'
 
-# @stub.local_entrypoint()
-# def main():
-#     print(f.local(1000))
+        row = {'issue_type':issue_type,'key':key,'summary':summary,'priority':priority,'product':product,'component':component,'created':created,'sla_escalation':sla_escalation, 'resolved':resolved, 'due':due, 'status':status}
+        rows.append(row)
+        df = pl.DataFrame(rows)
+    return df
 
-#     print(f.remote(999))
-
-#     total = 0
-#     for ret in f.map(range(20)):
-#         total += ret
-#     print(total)
 
 @flow
 def main():
-    print('hello world')
+    print(get_data())
 
 if __name__ == main():
     main()
